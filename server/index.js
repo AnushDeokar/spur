514f658643aec208b925b9f459000dc75134a95b
AnushDeokar <deokaranush@gmail.com>
2023-06-19 19:37:56 +0530

    updated title and favicon

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const path = require('path')
 var cookies = require("cookie-parser");
 
 // middlewares
-app.use( cors({
+app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: "https://spur-challenge.onrender.com/",
 }));
 app.use(express.json());
 app.use('/auth', authrouter);
